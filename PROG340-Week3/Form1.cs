81d2f9b3aff78adecdcc3cf3663293a035ec5ccf
agart001 <113141664+agart001@users.noreply.github.com>
2023-09-27 22:46:13 -0500

    added title

--- PROG340-Week3/Form1.cs
+++ PROG340-Week3/Form1.cs
@@ -1,3 +1,5 @@
+using System.Windows.Forms.VisualStyles;
+
 namespace PROG340_Week3
 {
     public partial class Form1 : Form
@@ -13,25 +15,49 @@ namespace PROG340_Week3
         {
             Graphics graphics = e.Graphics;
             Pen pen = new Pen(Color.Black);
+            SolidBrush brush = new SolidBrush(Color.Black);
+            Font titlefont = new Font("Arial", 20f);
+            StringFormat sformat = new StringFormat();
 
-            graphics.FillRectangle(new SolidBrush(Color.Azure), new Rectangle(0, 0, 800, 800));
+            brush.Color = Color.Azure;
+            graphics.FillRectangle(brush, new Rectangle(0, 0, 800, 800));
 
-            graphics.FillPie(new SolidBrush(Color.Orange), new Rectangle(600, -200, 400, 400), 90, 90);
+            brush.Color = Color.Orange;
+            graphics.FillPie(brush, new Rectangle(600, -200, 400, 400), 90, 90);
 
-            graphics.DrawArc(new Pen(Color.Yellow, 20f), new Rectangle(600, -200, 400, 400), 90, 90);
+            pen.Color = Color.Yellow;
+            pen.Width = 20f;
+            graphics.DrawArc(pen, new Rectangle(600, -200, 400, 400), 90, 90);
 
-            graphics.FillPolygon(new SolidBrush(Color.DarkGray), new Point[] { new Point(-100, 800), new Point(200, 200), new Point(500, 800)});
-            graphics.FillPolygon(new SolidBrush(Color.Gray), new Point[] { new Point(100, 800), new Point(400, 300), new Point(700, 800) });
+            brush.Color = Color.DarkGray;
+            graphics.FillPolygon(brush, new Point[] { new Point(-100, 800), new Point(200, 200), new Point(500, 800)});
 
-            graphics.FillEllipse(new SolidBrush(Color.DarkGreen), new Rectangle(-100, 580, 1000, 400));
+            brush.Color = Color.Gray;
+            graphics.FillPolygon(brush, new Point[] { new Point(100, 800), new Point(400, 300), new Point(700, 800) });
 
-            graphics.FillRectangle(new SolidBrush(Color.BlanchedAlmond), new Rectangle(370, 540, 50, 50));
-            graphics.DrawRectangle(new Pen(Color.Black, 5f), new Rectangle(370, 540, 50, 50));
+            brush.Color = Color.DarkGreen;
+            graphics.FillEllipse(brush, new Rectangle(-100, 580, 1000, 400));
 
-            graphics.FillPolygon(new SolidBrush(Color.BurlyWood), new Point[] { new Point(400, 550), new Point(410, 560), new Point(400, 580), new Point(390, 580), new Point(380, 560), new Point(390, 550) });
-            graphics.DrawPolygon(new Pen(Color.Black, 5f), new Point[] { new Point(400, 550), new Point(410, 560), new Point(400, 580), new Point(390, 580), new Point(380, 560), new Point(390, 550)});
+            brush.Color = Color.BlanchedAlmond;
+            graphics.FillRectangle(brush, new Rectangle(370, 540, 50, 50));
 
-            graphics.DrawEllipse(pen, new Rectangle(400, 510, 100, 100));
+            pen.Color = Color.Black;
+            pen.Width = 5f;
+            graphics.DrawRectangle(pen, new Rectangle(370, 540, 50, 50));
+
+            brush.Color = Color.BurlyWood;
+            graphics.FillPolygon(brush, new Point[] { new Point(400, 550), new Point(410, 560), new Point(400, 580), new Point(390, 580), new Point(380, 560), new Point(390, 550) });
+
+            graphics.DrawPolygon(pen, new Point[] { new Point(400, 550), new Point(410, 560), new Point(400, 580), new Point(390, 580), new Point(380, 560), new Point(390, 550)});
+
+            brush.Color = Color.BlueViolet;
+            sformat.Alignment = StringAlignment.Center;
+            graphics.DrawString("Gaming Game for Gamers", titlefont, brush, new RectangleF(200, 200, 400, 400), sformat);
+
+            pen.Color = Color.BlueViolet;
+            pen.Width = titlefont.Size / 2;
+            graphics.DrawLine(pen, new Point(225, 250), new Point(575, 250));
+
         }
     }
 }
